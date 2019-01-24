4858ff76ec18be41d0824858007e0bef7728cde2
punctual-sailor-2214 <bchett286@yahoo.com>
2019-01-24 23:05:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,8 +17,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 
 function deleteFromObjectByKey(object, key) {
   var newObj = Object.assign({}, object);
-  delete newObj[key];
-  return object;
+  return newObj[key];
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
